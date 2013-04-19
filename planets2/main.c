3ebf116844630758c8ed73fbeeeaae59f415700d
Hugo Valk <hugo.valk@gmail.com>
2013-04-20 00:08:57 +0200

    Initial Commit

---
+++ planets2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  planets2
+//
+//  Created by Hugo Valk on 4/20/13.
+//  Copyright (c) 2013 Dev Discoveries. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
